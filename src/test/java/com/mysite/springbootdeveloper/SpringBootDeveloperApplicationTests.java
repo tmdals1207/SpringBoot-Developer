a1164d817cefbac993a82014300f6319956be383
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-04-23 13:12:58 +0900

    [Feat] 프로젝트 초기 설정

---
+++ src/test/java/com/mysite/springbootdeveloper/SpringBootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
