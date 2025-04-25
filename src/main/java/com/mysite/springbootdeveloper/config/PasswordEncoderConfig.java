23290ad521f4fcac5f2f56eeca091e18d99fb1c5
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-04-25 14:37:55 +0900

    [Fix] BCryptPasswordEncoder 가 순환참조를 일으키는 문제를 BCryptPasswordEncoder 를 분리하여 해결

--- src/main/java/com/mysite/springbootdeveloper/config/PasswordEncoderConfig.java
+++ src/main/java/com/mysite/springbootdeveloper/config/PasswordEncoderConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfig {
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
